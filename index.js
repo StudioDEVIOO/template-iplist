7578730b4bb76053b171f786cebbbeedc6b1aa9c
alphaleadership <thomas.iniguez@free.fr>
2021-03-10 15:06:22 +0100

    dernier patch

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ logger = function(chunk){
         for (let i = 0; i < 256; i++) {
             for (let inde = 0; inde < 256; inde++) {
                 for (let ind = 0; ind < 256; ind++) {
-                    if (verif(`${index}.${i}.${inde}.${ind}`) = error) return ;
+                    if (verif(`${index}.${i}.${inde}.${ind}`) == 'error') return ;
                     else  logger(verif(`${index}.${i}.${inde}.${ind}`))
                     
     
