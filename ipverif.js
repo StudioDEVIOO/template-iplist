d6e0f62a8312be9879f68bf6e8d5173878e0b9bd
alphaleadership <thomas.iniguez@free.fr>
2021-03-10 14:58:49 +0100

    update
     ignore les ip inéxistante en béta

--- ipverif.js
+++ ipverif.js
@@ -1,22 +1,24 @@
 const axios = require('axios');
 
-const requests = (ip) => {
-    return axios({
+const requests = (ip) => new Promise((resolve, reject) => {
+    axios({
         method: 'GET',
-        url: `http://ip-api.com/json/${ip}?fields=status,as,,continent,country,region,regionName,city,zip,lat,lon,mobile,proxy,query`
+        url: `http://ip-api.com/json/${ip}?fields=status,as,continent,country,region,regionName,city,zip,lat,lon,mobile,proxy,query`
     })
         .then(res => {
-            if (res.data.status === 'fail') return;
-            return res.data;
-        });
-};
+            if (res.data.status === 'fail')
+                return reject( 'error');
+            
+            return resolve(res.data);
+        })
+        .catch(reject)
+});
 
 
 const get = async (ip) => {
     const result = await requests(ip)
-    console.log(result);
-   
-  
+    console.log(result); 
 };
+
 get('0.0.0.1');
 module.exports = get 
